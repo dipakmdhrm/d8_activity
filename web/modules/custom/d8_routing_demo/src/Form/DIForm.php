0729e5921dee3e78e118619f9f7512b0ff7e8992
Dipak Yadav <dipakmdhrm@gmail.com>
2018-09-25 23:23:19 +0530

    Activity 18: Form API Extended: #states & AJAX.

--- web/modules/custom/d8_routing_demo/src/Form/DIForm.php
+++ web/modules/custom/d8_routing_demo/src/Form/DIForm.php
@@ -40,7 +40,7 @@ class DIForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $last_entry = $this->dataFetcherWriter->getLastEntry();
 
-    $form ['first_name'] = [
+    $form['first_name'] = [
       '#type' => 'textfield',
       '#title' => t('First Name'),
       '#size' => 40,
@@ -49,13 +49,82 @@ class DIForm extends FormBase {
       '#default_value' => !empty($last_entry->first_name) ? $last_entry->first_name : '',
     ];
 
-    $form ['last_name'] = [
+    $form['last_name'] = [
       '#type' => 'textfield',
       '#title' => t('Last Name'),
       '#size' => 40,
       '#maxlength' => 40,
       '#required' => TRUE,
       '#default_value' => !empty($last_entry->last_name) ? $last_entry->last_name : '',
+    ];
+
+    $form['qualification'] = [
+      '#type' => 'select',
+      '#title' => t('Qualification'),
+      '#options' => array(
+        'ug' => t('UG'),
+        'pg' => t('PG'),
+        'other' => t('Other'),
+      ),
+    ];
+
+    $form['other'] = [
+      '#type' => 'textfield',
+      '#title' => t('If other, specify'),
+      '#size' => 40,
+      '#maxlength' => 40,
+      '#states' => array(
+        'visible' => array(
+          ':input[name="qualification"]' => array('value' => 'other'),
+        ),
+      ),
+    ];
+
+    $form['country'] = [
+      '#type' => 'select',
+      '#title' => t('Country'),
+      '#options' => array(
+        'india' => t('India'),
+        'uk' => t('UK'),
+      ),
+    ];
+
+    $form['india-states'] = [
+      '#type' => 'select',
+      '#title' => t('State'),
+      '#options' => array(
+        'jk' => t('J&K'),
+        'pb' => t('Punjab'),
+        'mh' => t('Maharashtra')
+      ),
+      '#states' => array(
+        'visible' => array(
+          ':input[name="country"]' => array('value' => 'india'),
+        ),
+      ),
+    ];
+
+    $form['uk-states'] = [
+      '#type' => 'select',
+      '#title' => t('State'),
+      '#options' => array(
+        'london' => t('London'),
+        'dorset' => t('Dorset'),
+        'sussex' => t('Sussex')
+      ),
+      '#states' => array(
+        'visible' => array(
+          ':input[name="country"]' => array('value' => 'uk'),
+        ),
+      ),
+    ];
+
+    $state = \Drupal::state();
+
+    $form['last_submitted'] = [
+      '#markup' => $this->t('Last submitted at: @time', [
+        '@time' => $state->get('d8_routing_demo.di_form_last_submitted'),
+      ]),
     ];
 
     $form['submit'] = [
@@ -75,6 +144,9 @@ class DIForm extends FormBase {
 
     try {
       $this->dataFetcherWriter->submitEntry($firstName, $lastName);
+      $state = \Drupal::state();
+      $state
+        ->set('d8_routing_demo.di_form_last_submitted', REQUEST_TIME);
     }
     catch (Exception $e) {
       $this->messenger()->addMessage(
