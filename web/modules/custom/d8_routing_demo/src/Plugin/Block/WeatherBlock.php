16edfecb06c322afd2ddbcf72f92c988309e91d8
Dipak Yadav <dyadav@ashoka.org>
2019-01-24 00:44:23 +0530

    Activity 20-part-3: Used http_client service to get weather data.

--- web/modules/custom/d8_routing_demo/src/Plugin/Block/WeatherBlock.php
+++ web/modules/custom/d8_routing_demo/src/Plugin/Block/WeatherBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Config\ConfigFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use GuzzleHttp\Client;
 
 /**
  * Provides a 'Weather Information' block.
@@ -17,10 +18,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class WeatherBlock extends BlockBase Implements ContainerFactoryPluginInterface {
   protected $weatherConfig;
+  protected $httpClient;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactory $configFactory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactory $configFactory, Client $client) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->weatherConfig = $configFactory->get('d8_routing_demo.weather');
+    $this->httpClient = $client;
   }
   /**
    * {@inheritdoc}
@@ -30,15 +33,20 @@ class WeatherBlock extends BlockBase Implements ContainerFactoryPluginInterface 
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('http_client')
     );
   }
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $appId = $this->weatherConfig->get('app_id');
-    $build['#markup'] = $appId;
+    $client = new \GuzzleHttp\Client();
+    $request = $client->request('GET', 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22');
+    $response = json_decode($request->getBody());
+    $city = $response->name;
+    $temp = $response->main->temp - 270.15;
+    $build['#markup'] = "City: $city, Temperature: $temp C";
     return $build;
   }
 
