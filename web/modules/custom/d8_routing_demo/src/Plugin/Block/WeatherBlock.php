3b5f1d3d69de1d057bbdceef0963724126a5429c
Dipak Yadav <dyadav@ashoka.org>
2019-01-24 00:12:05 +0530

    Activity 20-part-2: Added DI to block to get value from config.

--- web/modules/custom/d8_routing_demo/src/Plugin/Block/WeatherBlock.php
+++ web/modules/custom/d8_routing_demo/src/Plugin/Block/WeatherBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\d8_routing_demo\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Config\ConfigFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Weather Information' block.
@@ -12,12 +15,30 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Weather Information"),
  * )
  */
-class WeatherBlock extends BlockBase {
+class WeatherBlock extends BlockBase Implements ContainerFactoryPluginInterface {
+  protected $weatherConfig;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactory $configFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->weatherConfig = $configFactory->get('d8_routing_demo.weather');
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build['#markup'] = 'Some text';
+    $appId = $this->weatherConfig->get('app_id');
+    $build['#markup'] = $appId;
     return $build;
   }
 
